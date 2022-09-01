a73c041d481f179d88a8c99e1d0b447b5c1bef0f
Eshan <eshanjain845@gmail.com>
2022-09-01 19:28:38 +0530

    Added classes details in UI

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ mongoose.connect("mongodb://localhost/attendance_tracker");
 const user = require("./Schemas/user");
 const subject = require("./Schemas/subject");
 const app = express();
-
 const port = 3002;
 
 function calculatepercent(a,b){
@@ -32,10 +31,12 @@ app.get("/add",(req,res)=>{
     res.status(200).render("add.ejs");
 })
 
-app.post("/subject/add",(req,res)=>{
+app.post("/subject/add", async (req,res)=>{
+    subject.find({name:req.body.subjectName}).then(()=>{
+        return;
+    })
     subject.create({name:req.body.subjectName,attendance:0,percentage:"0%",totalClass:0});
-
-    res.redirect("/");
+    res.redirect("/add");
 })
 
 app.post("/attend/:id",async (req,res)=>{
@@ -60,6 +61,12 @@ app.post("/missed/:id",async (req,res)=>{
     await subject.updateOne({_id:req.params.id},{percentage:percentage}); 
     await subject.updateOne({_id:req.params.id},{totalClass:totalClass}); 
     res.redirect("/")
+});
+
+app.post("/subject/:id/delete",async (req,res)=>{
+    await subject.deleteOne({_id:req.params.id});
+    res.redirect("/")
+
 })
 
 app.listen(port,() => console.log("Server is running on port "+[port]));
