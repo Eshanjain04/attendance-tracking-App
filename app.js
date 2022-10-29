bd3bdfee17e97d25c3a1a653b9c4dae8c1370f9c
Eshan <eshanjain845@gmail.com>
2022-10-30 00:10:08 +0530

    changed route for reactjs

--- app.js
+++ app.js
@@ -11,6 +11,41 @@ const port = process.env.PORT || 8080;
 const bcrypt = require("bcrypt");
 const session = require("express-session");
 const middleware = require("./middleware");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+const secret = process.env.SECRET;
+
+app.use(cors());
+
+app.use("/student",(req,res,next)=>{
+    try{
+        const token = req.headers.authorization;
+        const decoded = jwt.verify(token,secret);
+        if(decoded){
+            req.user = decoded.data;
+            next();
+        }else{
+            res.json({message:"Not Authorized"})
+        }
+   }catch(e){
+        res.status(400).json({message:e.message});
+   }
+})
+
+app.use("/subject/add",(req,res,next)=>{
+    try{
+        const token = req.headers.authorization;
+        const decoded = jwt.verify(token,secret);
+        if(decoded){
+            req.user = decoded.data;
+            next();
+        }else{
+            res.json({message:"Not Authorized"})
+        }
+   }catch(e){
+        res.status(400).json({message:e.message});
+   }
+})
 
 function calculatepercent(a,b){
     return `${parseInt((a/(b))*100)}%`
@@ -35,12 +70,17 @@ app.get("/add",(req,res)=>{
 })
 
 app.post("/subject/add", async (req,res)=>{
-    subject.find({name:req.body.subjectName}).then(()=>{
-        return;
-    });
-
-    await subject.create({name:req.body.subjectName,attendance:0,percentage:"0%",totalClass:0,users:req.session.user._id});
-    res.redirect("/add");
+    try{
+        subject.find({name:req.body.subjectName}).then(()=>{
+            return;
+        });
+    
+        await subject.create({name:req.body.subjectName,attendance:0,percentage:"0%",totalClass:0,users:req.user});
+        res.status(201).json({message:"Created"})
+    }
+    catch(e){
+        res.status(400).json({message:e.message});
+    }
 })
 
 app.post("/attend/:id",async (req,res)=>{
@@ -53,7 +93,7 @@ app.post("/attend/:id",async (req,res)=>{
     await subject.updateOne({_id:req.params.id},{attendance:updatedAttendance});
     await subject.updateOne({_id:req.params.id},{percentage:percentage}); 
     await subject.updateOne({_id:req.params.id},{totalClass:totalClass}); 
-    res.redirect("/")
+    res.json({message:"missed"})
 })
 
 app.post("/missed/:id",async (req,res)=>{
@@ -64,13 +104,12 @@ app.post("/missed/:id",async (req,res)=>{
     const percentage = calculatepercent(previousAttendance,totalClass);
     await subject.updateOne({_id:req.params.id},{percentage:percentage}); 
     await subject.updateOne({_id:req.params.id},{totalClass:totalClass}); 
-    res.redirect("/")
+    res.json({message:"Attended"})
 });
 
-app.post("/subject/:id/delete",async (req,res)=>{
+app.delete("/subject/:id/delete",async (req,res)=>{
     await subject.deleteOne({_id:req.params.id});
     res.redirect("/")
-
 });
 
 app.get("/register",async (req,res)=>{
@@ -84,34 +123,48 @@ app.post("/register" ,async(req,res)=>{
         var email = req.body.email;
         var password = req.body.password;
 
-        password = await bcrypt.hash(password,10);
-        req.body.password = password;
-        await user.create({
-            firstName:firstName,
-            lastName : lastName,
-            email:email,
-            password:password,
-        });
+        const userData = await user.findOne({email:email});
+        if(!userData){
+            password = await bcrypt.hash(password,10);
+            req.body.password = password;
+            await user.create({
+                firstName:firstName,
+                lastName : lastName,
+                email:email,
+                password:password,
+            });
+            const data = await user.findOne({email:email});
+            const token = jwt.sign({
+                exp: Math.floor(Date.now() / 1000) + (60 * 60),
+                data: data._id
+              }, secret);
 
-        var userData = await user.findOne({email:email});
-        req.session.user = userData;
-        
-        res.redirect("/");
+              res.status(200).json({token:token})
+        }else{
+            res.status(400).json({message:"User Already Exists"});
+        }
+
+
     }catch(e){
-        console.log(e);
+        res.status(400).json({message:e.message});
     }
     
 });
 
-app.get("/",middleware.requireLogin,async (req,res)=>{
+app.get("/student",async (req,res)=>{
     subject.find().populate("users").then(data=>{
         var subject_name = new Set();
         for(var i of data){
-            if(i.users._id.valueOf()===req.session.user._id){
+            if(i.users._id.valueOf()===req.user){
                 subject_name.add(i);
             }
         }
-        res.render("home",{subject_name});
+        let finalArr = [];
+        for(let i of subject_name){
+            finalArr.push(i);
+        }
+        console.log(subject_name);
+        res.status(200).json({data:finalArr});
     })
     
 
@@ -129,12 +182,18 @@ app.post("/login",async(req,res)=>{
     if(userData != null){
         var result = await bcrypt.compare(password,userData.password);
         if(result===true){
-            req.session.user = userData;
-            return res.redirect("/");
-        }
-    }
+            const token = jwt.sign({
+                exp: Math.floor(Date.now() / 1000) + (60 * 60),
+                data: userData._id
+              }, secret);
 
-    res.status(400).json({message:"UserData not Found"});
+              res.status(200).json({token:token})
+        }else{
+            res.status(400).json({message:"Password Does not match"})
+        }
+    }else{
+        res.status(400).json({message:"User not Found"})
+    }
 });
 
 app.get("/logout",(req,res,next)=>{
--- middleware.js
+++ middleware.js
@@ -1,8 +1,19 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const secret = process.env.SECRET;
+
 exports.requireLogin = (req,res,next) =>{
-    if(req.session && req.session.user){
-        return next();
-    }
-    else{
-        return res.redirect("/login");
-    }
+   try{
+        const token = req.headers.Authorization;
+        console.log(token);
+        const decoded = jwt.verify(token,secret);
+        if(decoded){
+            req.user = decoded.data;
+            next();
+        }else{
+            res.json({message:"Not Authorized"})
+        }
+   }catch(e){
+        res.status(400).json({message:e.message});
+   }
 } 
